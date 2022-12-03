7c3d12fa5252cfe14804aee33aaafd2f358e05be
Nataly <basnaly@gmail.com>
2022-12-03 08:18:57 +0200

    Add discounts

--- app/controllers/cart.controller.js
+++ app/controllers/cart.controller.js
@@ -1,4 +1,5 @@
 const db = require("../models");
+const Item = require("../models/item.model");
 const Cart = db.cart;
 
 exports.updateCart = async (req, res) => {
@@ -35,17 +36,42 @@ exports.getCartList = async (req, res) => {
             createUser: req.userId 
         }).select('listCartItems').exec()
 
+        const discountItems = await Item.find().gt
+            ('discount.discountPrice', 0)
+         console.log(discountItems)   
+        
         let mappedCartList = result.listCartItems.map(el => {
+
+            const discountItem = discountItems.find(
+                (item) =>
+                    el?.itemName === item?.itemName &&
+                    el?.quantity >= item?.discount?.discountAmount
+            );
+            console.log(discountItem)
+
+            let correctedPrice = el.price
+
+            let correctedTotal = el.total
+
+            if (discountItem) {
+
+                correctedPrice = discountItem?.discount?.discountPrice /
+                    discountItem?.discount?.discountAmount
+
+                correctedTotal = correctedPrice * el?.quantity
+            }
+
             return {
                 id: el._id,
                 createUser: el.createUser,
                 itemName: el.itemName,
                 image: el.image,
-                price: el.price,
+                price: correctedPrice,
                 unit: el.unit,
                 note: el.note,
                 quantity: el.quantity,
-                total: el.total
+                total: correctedTotal,
+                isDiscount: !!discountItem
             }
         })
 
